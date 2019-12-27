60ec03b697e1a39024cb194905c6bba2f99e2c37
ddz <ddzyan@163.com>
2019-12-27 11:40:36 +0800

    去除无关默认配置

--- config/config.unittest.js
+++ config/config.unittest.js
@@ -2,18 +2,7 @@
 
 module.exports = () => {
   const config = {
-    nodeConfig: {
-      httpEndpoint: 'http://10.199.5.219:8888',
-      broadcast: false,
-      sign: true,
-      expireSeconds: 60,
-      blocksBehind: 3,
-      systemContract: 'eosio.token',
-      systemActive: 'transfer',
-      systemPermission: 'active',
-      systemSymbol: 'EOS',
-      privateKeys: ['5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3'],
-    },
+
   };
 
   return config;
